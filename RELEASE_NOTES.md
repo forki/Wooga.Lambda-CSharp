65520765f966314e7e71bcc38ef73884139becfc
devboy <dominic.graefen@gmail.com>
2015-03-21 07:24:48 +0100

    Bump version to 0.2.0

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,2 +1,8 @@
+#### 0.2.0 - March 19 2015
+* Splits monadic functions and their result types in different files.
+* Adds Wooga.Lambda.Log project
+* Adds Wooga.Lambda.FileSystem project
+* Adds Wooga.Lambda.Network project
+
 #### 0.1.0 - March 19 2015
 * Initial release
--- src/Wooga.Lambda.Network/Properties/AssemblyInfo.cs
+++ src/Wooga.Lambda.Network/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTitleAttribute("Wooga.Lambda.Network")]
 [assembly: AssemblyProductAttribute("Wooga.Lambda")]
 [assembly: AssemblyDescriptionAttribute("Functional utilities for C#")]
-[assembly: AssemblyVersionAttribute("0.1.0")]
-[assembly: AssemblyFileVersionAttribute("0.1.0")]
+[assembly: AssemblyVersionAttribute("0.2.0")]
+[assembly: AssemblyFileVersionAttribute("0.2.0")]
 [assembly: InternalsVisibleToAttribute("Wooga.Lambda.Network.Tests")]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "0.1.0";
+        internal const string Version = "0.2.0";
     }
 }
