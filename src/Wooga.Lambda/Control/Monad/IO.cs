91078f8e5c67365e9bee6a68ede333b0b86ab2c4
devboy <dominic.graefen@gmail.com>
2015-04-22 13:57:17 +0200

    Commenting out that weird wannabe do-notation

--- src/Wooga.Lambda/Control/Monad/IO.cs
+++ src/Wooga.Lambda/Control/Monad/IO.cs
@@ -11,21 +11,21 @@ namespace Wooga.Lambda.Control.Monad
 
     public static class IO
     {
-        public static TReturn Do<T1, T2, TReturn>(IO<T1> m, IO<T2> h, Func<T1, T2, TReturn> f)
-        {
-            return f(m(), h());
-        }
-
-        public static TReturn Do<T1, T2, T3, TReturn>(IO<T1> m, IO<T2> h, IO<T3> i, Func<T1, T2, T3, TReturn> f)
-        {
-            return f(m(), h(), i());
-        }
-
-        public static TReturn Do<T1, T2, T3, T4, TReturn>(IO<T1> m, IO<T2> h, IO<T3> i, IO<T4> k,
-            Func<T1, T2, T3, T4, TReturn> f)
-        {
-            return f(m(), h(), i(), k());
-        }
+//        public static TReturn Do<T1, T2, TReturn>(IO<T1> m, IO<T2> h, Func<T1, T2, TReturn> f)
+//        {
+//            return f(m(), h());
+//        }
+//
+//        public static TReturn Do<T1, T2, T3, TReturn>(IO<T1> m, IO<T2> h, IO<T3> i, Func<T1, T2, T3, TReturn> f)
+//        {
+//            return f(m(), h(), i());
+//        }
+//
+//        public static TReturn Do<T1, T2, T3, T4, TReturn>(IO<T1> m, IO<T2> h, IO<T3> i, IO<T4> k,
+//            Func<T1, T2, T3, T4, TReturn> f)
+//        {
+//            return f(m(), h(), i(), k());
+//        }
 
         public static IO<T2> Bind<T1, T2>(this IO<T1> m, Func<T1, IO<T2>> f)
         {
--- tests/Wooga.Lambda.Tests/System/IOTests.cs
+++ tests/Wooga.Lambda.Tests/System/IOTests.cs
@@ -6,20 +6,20 @@ namespace Wooga.Lambda.Tests.System
     [TestFixture]
     public class IOTests
     {
-        [Test]
-        public void DoAppliesParameters()
-        {
-            var one = IO.Return(() => "1");
-            var two = IO.Return(() => "2");
-            var tre = IO.Return(() => "3");
-            var fur = IO.Return(() => "4");
-            var x = IO.Do(one, two, (a,b) => a + b);
-            var y = IO.Do(one, two, tre, (a, b, c) => a + b + c);
-            var z = IO.Do(one, two, tre, fur, (a, b, c, d) => a + b + c + d);
-            Assert.AreEqual("12", x);
-            Assert.AreEqual("123", y);
-            Assert.AreEqual("1234", z);
-        }
+//        [Test]
+//        public void DoAppliesParameters()
+//        {
+//            var one = IO.Return(() => "1");
+//            var two = IO.Return(() => "2");
+//            var tre = IO.Return(() => "3");
+//            var fur = IO.Return(() => "4");
+//            var x = IO.Do(one, two, (a,b) => a + b);
+//            var y = IO.Do(one, two, tre, (a, b, c) => a + b + c);
+//            var z = IO.Do(one, two, tre, fur, (a, b, c, d) => a + b + c + d);
+//            Assert.AreEqual("12", x);
+//            Assert.AreEqual("123", y);
+//            Assert.AreEqual("1234", z);
+//        }
     }
 
     [TestFixture]
