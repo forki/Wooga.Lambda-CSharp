c73b4e1c31dc4efc667ec216597fb74469ec9947
devboy <dominic.graefen@gmail.com>
2015-04-24 19:16:49 +0200

    Adds very basic pattern matching

---
+++ src/Wooga.Lambda/Control/PatternMatching/Pattern.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Wooga.Lambda.Control.Monad;
+
+namespace Wooga.Lambda.Control.PatternMatching
+{
+    public delegate Either<TResult, TValue> PatterMatch<TValue, TResult>();
+
+    public static class Pattern
+    {
+        public static PatterMatch<TValue, TResult> Match<TValue, TResult>(TValue x)
+        {
+            return () => Either.Right<TResult, TValue>(x);
+        }
+
+        public static PatterMatch<TValue, TResult> Case<TValue, TResult>(this PatterMatch<TValue, TResult> m, Func<TValue, bool> t, Func<TValue, TResult> f)
+        {
+            return () => m().Bind(y => t(y) ? Either.Left<TResult, TValue>(f(y)) : Either.Right<TResult, TValue>(y));
+        }
+
+        public static PatterMatch<TValue, TResult> Default<TValue, TResult>(this PatterMatch<TValue, TResult> m, Func<TResult> f)
+        {
+            return Case(m,_ => true, _ => f());
+        }
+
+        public static TResult Evaluate<TValue, TResult>(this PatterMatch<TValue, TResult> m)
+        {
+            return m().FromLeft();
+        }
+
+    }
+}
---
+++ tests/Wooga.Lambda.Tests/Control/PatternMatching/PatternMatchingTests.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using Wooga.Lambda.Control.PatternMatching;
+
+namespace Wooga.Lambda.Tests.Control.PatternMatching
+{
+    [TestFixture]
+    public class PatternMatchingTests
+    {
+        [Test]
+        public static void ChooseDefaultWhenNoMatch()
+        {
+            var t = Pattern
+                    .Match<string, string>("hello")
+                    .Case(s => s == "cat", s => s)
+                    .Default(()=> "dog")
+                    .Evaluate();
+            Assert.AreEqual("dog", t);
+        }
+
+        [Test]
+        public static void ChooseFirstOfMultipleCorrectMatch()
+        {
+            var t = Pattern
+                    .Match<string, string>("cat")
+                    .Case(s => s == "cat", s => s)
+                    .Case(s => s == "cat", _ => "dog")
+                    .Evaluate();
+            Assert.AreEqual("cat", t);
+        }
+
+        [Test]
+        public static void ChooseFirstCorrectMatch()
+        {
+            var t = Pattern
+                    .Match<string, string>("cat")
+                    .Case(s => s == "fish", s => s)
+                    .Case(s => s == "cat", _ => "dog")
+                    .Evaluate();
+            Assert.AreEqual("dog", t);
+        }
+
+        [Test]
+        public static void ChooseFirstCorrectMatchAboveDefault()
+        {
+            var t = Pattern
+                    .Match<string, string>("cat")
+                    .Case(s => s == "fish", s => s)
+                    .Case(s => s == "cat", _ => "dog")
+                    .Default(() => "default")
+                    .Evaluate();
+            Assert.AreEqual("dog", t);
+        }
+
+    }
+}
