1001eadc849ac9b65ba4948188396efd8d98f3e7
devboy <dominic.graefen@gmail.com>
2015-07-24 23:45:42 +0200

    Adds TestSuite for Storage library

---
+++ tests/Wooga.Lambda.Storage.Tests/FileSystem/LocalFileSystemTests.cs
@@ -0,0 +1,9 @@
+﻿using NUnit.Framework;
+
+namespace Wooga.Lambda.Storage.Tests.FileSystem
+{
+    [TestFixture]
+    public class LocalFileSystemTests
+    {
+    }
+}
---
+++ tests/Wooga.Lambda.Storage.Tests/FileSystem/VirtualFileSystemTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using Wooga.Lambda.Data;
+using Wooga.Lambda.Storage.FileSystem;
+
+namespace Wooga.Lambda.Storage.Tests.FileSystem
+{
+    [TestFixture]
+    public class VirtualFileSystemTests
+    {
+        [Test]
+        public void ShouldAddFile()
+        {
+            var vfs = VirtualFileSystem.Virtual();
+            var location = vfs.Locate("a.b");
+            vfs.WriteFileAsync(location, ImmutableList.Empty<byte>())();
+            Assert.IsTrue(vfs.HasFileAsync(location)());
+        }
+
+        [Test]
+        public void ShouldRemoveFile()
+        {
+            var vfs = VirtualFileSystem.Virtual();
+            var location = vfs.Locate("a.b");
+            vfs.WriteFileAsync(location, ImmutableList.Empty<byte>())();
+            Assert.IsTrue(vfs.HasFileAsync(location)());
+            vfs.RmFileAsync(location)();
+            Assert.IsFalse(vfs.HasFileAsync(location)());
+        }
+    }
+}
