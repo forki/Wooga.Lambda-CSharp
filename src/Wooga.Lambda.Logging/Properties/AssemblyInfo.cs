34bbcf5e8ad2679f417a3e2b1d0078654de2a62d
devboy <dominic.graefen@gmail.com>
2015-08-13 12:00:10 +0200

    Bump version to 0.6.5

--- src/Wooga.Lambda.Logging/Properties/AssemblyInfo.cs
+++ src/Wooga.Lambda.Logging/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTitleAttribute("Wooga.Lambda.Logging")]
 [assembly: AssemblyProductAttribute("Wooga.Lambda")]
 [assembly: AssemblyDescriptionAttribute("Functional primitives for Unity3D")]
-[assembly: AssemblyVersionAttribute("0.6.4")]
-[assembly: AssemblyFileVersionAttribute("0.6.4")]
+[assembly: AssemblyVersionAttribute("0.6.5")]
+[assembly: AssemblyFileVersionAttribute("0.6.5")]
 [assembly: InternalsVisibleToAttribute("Wooga.Lambda.Logging.Tests")]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "0.6.4";
+        internal const string Version = "0.6.5";
     }
 }
